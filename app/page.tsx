3b73e5317d7a3c76b981da15482c19098ed7cb58
ricokurnia12 <ricokurnia19@gmail.com>
2026-03-09 20:04:57 +0700

    crete login service

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import FloatingChat from '@/components/layout/floatingChat'
 import Home from '@/components/pages/home/home'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
+    <FloatingChat/>
     <Home/>
+    </>
   )
 }
 
